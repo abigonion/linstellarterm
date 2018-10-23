06cfef4c3afb86bafa899fb6f44b28239aa13b3f
DESKTOP-PQ9G3UI\nihao <994776610@qq.com>
2018-10-23 16:19:44 +0800

    no message

--- directory/directory.js
+++ directory/directory.js
@@ -21,18 +21,665 @@ directory.DirectoryBuilder = DirectoryBuilder;
 // });
 
 directory.addAnchor({
+  domain: 'ximcoin.com',
+  website: 'https://ximcoin.com',
+  logo: 'ximcoin.com',
+  color: '#000000',
+  displayName: 'XIM',
+});
+
+directory.addAsset('ximcoin.com', {
+  code: 'XIM',
+  issuer: 'GBZ35ZJRIKJGYH5PBKLKOZ5L6EXCNTO7BKIL7DAVVDFQ2ODJEEHHJXIM',
+  instructions: 'https://ximcoin.com',
+  type: 'token',
+});
+
+directory.addAnchor({
+  domain: 'ternio.io',
+  website: 'https://ternio.io',
+  logo: 'ternio.io',
+  color: '#1D344F',
+  displayName: 'TERN'
+});
+
+directory.addAsset('ternio.io', {
+  code: 'TERN',
+  issuer: 'GDGQDVO6XPFSY4NMX75A7AOVYCF5JYGW2SHCJJNWCQWIDGOZB53DGP6C',
+  type: 'token'
+});
+
+directory.addAnchor({
+  domain: 'six.network',
+  website: 'https://six.network',
+  logo: 'six.network',
+  color: '#454ca4',
+  displayName: 'SIX',
+});
+
+directory.addAsset('six.network', {
+  code: 'SIX',
+  issuer: 'GDMS6EECOH6MBMCP3FYRYEVRBIV3TQGLOFQIPVAITBRJUMTI6V7A2X6Z',
+  type: 'token'
+});
+
+directory.addAnchor({
+  domain: 'tontinetrust.com',
+  website: 'https://tontinetrust.com',
+  logo: 'tontinetrust.com',
+  color: '#2975bf',
+  displayName: 'TON Money',
+});
+
+directory.addAsset('tontinetrust.com', {
+  code: 'TON',
+  issuer: 'GBFJGO46OV6E2QS2ZUMCF256ZL4BFOZWFHULRNLPSPW47HH5HFAKJTON',
+  type: 'token',
+});
+
+directory.addAnchor({
+  domain: 'cryptotari.io',
+  website: 'https://cryptotari.io',
+  logo: 'cryptotari.io',
+  color: '#E31E24',
+  displayName: 'TARI',
+});
+
+directory.addAsset('cryptotari.io', {
+  code: 'TARI',
+  issuer: 'GD7UVDDJHJYKUXB4SJFIC6VJDQ4YADQCMRN3KLHJFV4H6NIUAEREVCO7',
+  type: 'token',
+});
+
+directory.addAnchor({
+  domain: 'frasindo.com',
+  website: 'https://frasindo.com',
+  logo: 'frasindo.com',
+  color: '#be3785',
+  displayName: 'Frasindo',
+});
+
+directory.addAsset('frasindo.com', {
+  code: 'FRAS',
+  issuer: 'GC75WHUIMU7LV6WURMCA5GGF2S5FWFOK7K5VLR2WGRKWKZQAJQEBM53M',
+  type: 'token',
+});
+
+directory.addAnchor({
+  domain: 'pedity.com',
+  website: 'https://pedity.com',
+  logo: 'pedity.com',
+  color: '#2a8bc6',
+  displayName: 'Pedity',
+});
+
+directory.addAsset('pedity.com', {
+  code: 'PEDI',
+  issuer: 'GBVUDZLMHTLMZANLZB6P4S4RYF52MVWTYVYXTQ2EJBPBX4DZI2SDOLLY',
+  instructions: 'https://pedity.com',
+  type: 'token',
+});
+
+directory.addAnchor({
+  domain: 'nezly.com',
+  website: 'https://nezly.com',
+  logo: 'nezly.com',
+  color: '#523b83',
+  displayName: 'Nezly',
+});
+
+directory.addAsset('nezly.com', {
+  code: 'NEZ',
+  issuer: 'GDGKBRCPW4C3ENNC5C64PE6U33MG52GBKFXOK5P3OSWF74DAOXRXV6OJ',
+  instructions: 'https://nezly.com',
+  unlisted: true,
+});
+
+directory.addAsset('nezly.com', {
+  code: 'ETH',
+  issuer: 'GDGKBRCPW4C3ENNC5C64PE6U33MG52GBKFXOK5P3OSWF74DAOXRXV6OJ',
+  instructions: 'https://nezly.com',
+  unlisted: true,
+});
+
+directory.addAsset('nezly.com', {
+  code: 'BTC',
+  issuer: 'GDGKBRCPW4C3ENNC5C64PE6U33MG52GBKFXOK5P3OSWF74DAOXRXV6OJ',
+  instructions: 'https://nezly.com',
+  unlisted: true,
+});
+
+directory.addAnchor({
+  domain: 'superlumen.org',
+  website: 'https://www.superlumen.org/',
+  logo: 'superlumen.org',
+  color: '#1c4664',
+  displayName: 'Superlumens',
+});
+directory.addAsset('superlumen.org', {
+  code: 'SLM',
+  issuer: 'GCEGERI7COJYNNID6CYSKS5DPPLGCCLPTOSCDD2LG5SJIVWM5ISUPERI',
+  type: 'token',
+});
+
+directory.addAnchor({
+  domain: 'smartlands.io',
+  website: 'https://smartlands.io',
+  logo: 'smartlands.io',
+  color: '#35AB62',
+  displayName: 'Smartlands',
+});
+directory.addAsset('smartlands.io', {
+  code: 'SLT',
+  issuer: 'GCKA6K5PCQ6PNF5RQBF7PQDJWRHO6UOGFMRLK3DYHDOI244V47XKQ4GP',
+  instructions: 'https://smartlands.io',
+  type: 'token',
+});
+
+directory.addAnchor({
+  domain: 'old.repocoin.io',
+  website: 'https://old.repocoin.io',
+  logo: 'old.repocoin.io',
+  color: '#192854',
+  displayName: 'Repocoin',
+});
+directory.addAsset('old.repocoin.io', {
+  code: 'REPO',
+  issuer: 'GDPB3BGHJAHAKVIWUNLST7N6OGZ73W6AUAI7QDZJW26LEWL46VDAKBH6',
+  instructions: 'https://old.repocoin.io',
+  type: 'token',
+});
+
+directory.addAnchor({
+  domain: 'repocoin.io',
+  website: 'https://repocoin.io',
+  logo: 'repocoin.io',
+  color: '#192854',
+  displayName: 'Repocoin',
+});
+directory.addAsset('repocoin.io', {
+  code: 'REPO',
+  issuer: 'GCZNF24HPMYTV6NOEHI7Q5RJFFUI23JKUKY3H3XTQAFBQIBOHD5OXG3B',
+  instructions: 'https://repocoin.io',
+  type: 'token',
+});
+
+directory.addAnchor({
+  domain: 'stemchain.io',
+  website: 'https://stemchain.io',
+  logo: 'stemchain.io',
+  color: '#1db5da',
+  displayName: 'STEMchain',
+});
+directory.addAsset('stemchain.io', {
+  code: 'STEM',
+  issuer: 'GAFXX2VJE2EGLLY3EFA2BQXJADAZTNR7NC7IJ6LFYPSCLE7AI3AK3B3M',
+  instructions: 'https://stemchain.io',
+  type: 'token',
+});
+
+directory.addAnchor({
+  domain: 'charnatoken.top',
+  website: 'https://charnatoken.top',
+  logo: 'charnatoken.top',
+  color: '#1c4664',
+  displayName: 'Charna Token',
+});
+directory.addAsset('charnatoken.top', {
+  code: 'CHRC',
+  issuer: 'GBRPTWEZTUKYM6VJXLHXBFI23M2GSY3TCVIQSZKFQLMOJXH7VPDGKBDP',
+});
+
+directory.addAnchor({
+  domain: 'irene.energy',
+  website: 'https://irene.energy',
+  logo: 'irene.energy',
+  color: '#38909a',
+  displayName: 'TELLUS',
+});
+directory.addAsset('irene.energy', {
+  code: 'TELLUS',
+  issuer: 'GBBRMEXJMS3L7Y3DZZ2AHBD545GZ72OAEHHEFKGZAHHASHGWMHK5P6PL',
+  instructions: 'https://irene.energy',
+  type: 'token',
+});
+
+directory.addAnchor({
+  domain: 'stronghold.co',
+  website: 'https://stronghold.co',
+  logo: 'stronghold.co',
+  color: '#13ce66',
+  displayName: 'Stronghold',
+});
+directory.addAsset('stronghold.co', {
+  code: 'BTC',
+  issuer: 'GBSTRH4QOTWNSVA6E4HFERETX4ZLSR3CIUBLK7AXYII277PFJC4BBYOG',
+  instructions: 'https://stronghold.co/send-receive-bitcoin.html',
+});
+directory.addAsset('stronghold.co', {
+  code: 'ETH',
+  issuer: 'GBSTRH4QOTWNSVA6E4HFERETX4ZLSR3CIUBLK7AXYII277PFJC4BBYOG',
+  instructions: 'https://stronghold.co/send-receive-ethereum.html',
+});
+directory.addAsset('stronghold.co', {
+  code: 'XLM',
+  issuer: 'GBSTRH4QOTWNSVA6E4HFERETX4ZLSR3CIUBLK7AXYII277PFJC4BBYOG',
+  instructions: 'https://stronghold.co/send-receive-lumens.html',
+  unlisted: true,
+});
+directory.addAsset('stronghold.co', {
+  code: 'USD',
+  issuer: 'GBSTRUSD7IRX73RQZBL3RQUH6KS3O4NYFY3QCALDLZD77XMZOPWAVTUK',
+  instructions: 'https://stronghold.co',
+});
+
+directory.addAnchor({
+  domain: 'naobtc.com',
+  website: 'https://naobtc.com/',
+  logo: 'naobtc.com',
+  color: '#08b5e5',
+  displayName: 'NaoBTC',
+});
+directory.addAsset('naobtc.com', {
+  code: 'BTC',
+  issuer: 'GATEMHCCKCY67ZUCKTROYN24ZYT5GK4EQZ65JJLDHKHRUZI3EUEKMTCH',
+  instructions: 'https://naobtc.com/#/howto/deposit',
+});
+directory.addAsset('naobtc.com', {
+  code: 'XEL',
+  issuer: 'GAXELY4AOIRVONF7V25BUPDNKZYIVT6CWURG7R2I6NQU26IQSQODBVCS',
+  instructions: 'https://naobtc.com/#/howto/deposit',
+});
+
+directory.addAnchor({
+  domain: 'vcbear.net',
+  website: 'https://vcbear.net/',
+  logo: 'vcbear.net',
+  color: '#0c88b6',
+  displayName: 'VCBear',
+});
+directory.addAsset('vcbear.net', {
+  code: 'JPY',
+  issuer: 'GBVAOIACNSB7OVUXJYC5UE2D4YK2F7A24T7EE5YOMN4CE6GCHUTOUQXM',
+  instructions: 'https://www.vcbear.net/signin',
+});
+directory.addAsset('vcbear.net', {
+  code: 'XRP',
+  issuer: 'GA7FCCMTTSUIC37PODEL6EOOSPDRILP6OQI5FWCWDDVDBLJV72W6RINZ',
+  instructions: 'https://www.vcbear.net/signin',
+});
+directory.addAsset('vcbear.net', {
+  code: 'BTC',
+  issuer: 'GDXTJEK4JZNSTNQAWA53RZNS2GIKTDRPEUWDXELFMKU52XNECNVDVXDI',
+  instructions: 'https://www.vcbear.net/signin',
+});
+
+directory.addAnchor({
+  domain: 'coins.asia',
+  website: 'https://coins.asia/',
+  logo: 'coins.asia',
+  color: '#04bcfc',
+  displayName: 'coins.asia',
+});
+directory.addAsset('coins.asia', {
+  code: 'PHP',
+  issuer: 'GBUQWP3BOUZX34TOND2QV7QQ7K7VJTG6VSE7WMLBTMDJLLAW7YKGU6EP',
+});
+
+
+directory.addAnchor({
+  domain: 'tempo.eu.com',
+  website: 'https://tempo.eu.com/',
+  logo: 'tempo.eu.com',
+  color: '#1b92de',
+  displayName: 'TEMPO',
+});
+directory.addAsset('tempo.eu.com', {
+  code: 'EURT',
+  issuer: 'GAP5LETOV6YIE62YAM56STDANPRDO7ZFDBGSNHJQIYGGKSMOZAHOOS2S',
+});
+
+directory.addAnchor({
+  domain: 'sureremit.co',
+  website: 'https://sureremit.co',
+  logo: 'sureremit.co',
+  color: '#363187',
+  displayName: 'SureRemit',
+});
+directory.addAsset('sureremit.co', {
+  code: 'RMT',
+  issuer: 'GDEGOXPCHXWFYY234D2YZSPEJ24BX42ESJNVHY5H7TWWQSYRN5ZKZE3N',
+  instructions: 'https://sureremit.co',
+  type: 'token',
+});
+
+
+directory.addAnchor({
+  domain: 'old.sureremit.co',
+  website: 'https://old.sureremit.co',
+  logo: 'old.sureremit.co',
+  color: '#dc5126',
+  displayName: 'SureRemit OLD',
+});
+directory.addAsset('old.sureremit.co', {
+  code: 'RMT',
+  issuer: 'GCVWTTPADC5YB5AYDKJCTUYSCJ7RKPGE4HT75NIZOUM4L7VRTS5EKLFN',
+  warning: 'ACTION REQUIRED: SureRemit.co has issued a token swap. For more information, visit https://swap.sureremit.co/ or visit official SureRemit communications. The old RMT will be delisted soon.',
+  disabled: true,
+});
+
+
+directory.addAnchor({
+  domain: 'ripplefox.com',
+  website: 'https://ripplefox.com/anchor/',
+  logo: 'ripplefox.com',
+  color: '#fc682a',
+  displayName: 'RippleFox',
+});
+directory.addAsset('ripplefox.com', {
+  code: 'CNY',
+  issuer: 'GAREELUB43IRHWEASCFBLKHURCGMHE5IF6XSE7EXDLACYHGRHM43RFOX',
+  instructions: 'Leave your address in the message to seller when you order the item: https://shop109149722.taobao.com',
+});
+
+directory.addAnchor({
+  domain: 'apay.io',
+  website: 'https://apay.io/',
+  logo: 'papayame.com',
+  color: '#efa344',
+  displayName: 'apay.io',
+});
+directory.addAsset('apay.io', {
+  code: 'BCH',
+  issuer: 'GAEGOS7I6TYZSVHPSN76RSPIVL3SELATXZWLFO4DIAFYJF7MPAHFE7H4',
+  instructions: 'https://apay.io/',
+});
+directory.addAsset('apay.io', {
+  code: 'BTC',
+  issuer: 'GAUTUYY2THLF7SGITDFMXJVYH3LHDSMGEAKSBU267M2K7A3W543CKUEF',
+  instructions: 'https://apay.io/',
+});
+directory.addAsset('apay.io', {
+  code: 'LTC',
+  issuer: 'GC5LOR3BK6KIOK7GKAUD5EGHQCMFOGHJTC7I3ELB66PTDFXORC2VM5LP',
+  instructions: 'https://apay.io/',
+});
+directory.addAsset('apay.io', {
+  code: 'ICN',
+  issuer: 'GBDEVU63Y6NTHJQQZIKVTC23NWLQVP3WJ2RI2OTSJTNYOIGICST6DUXR',
+  instructions: 'https://apay.io/',
+});
+directory.addAsset('apay.io', {
+  code: 'ETH',
+  issuer: 'GBDEVU63Y6NTHJQQZIKVTC23NWLQVP3WJ2RI2OTSJTNYOIGICST6DUXR',
+  instructions: 'https://apay.io/',
+});
+directory.addAsset('apay.io', {
+  code: 'BAT',
+  issuer: 'GBDEVU63Y6NTHJQQZIKVTC23NWLQVP3WJ2RI2OTSJTNYOIGICST6DUXR',
+  instructions: 'https://apay.io/',
+});
+directory.addAsset('apay.io', {
+  code: 'OMG',
+  issuer: 'GBDEVU63Y6NTHJQQZIKVTC23NWLQVP3WJ2RI2OTSJTNYOIGICST6DUXR',
+  instructions: 'https://apay.io/',
+});
+directory.addAsset('apay.io', {
+  code: 'REP',
+  issuer: 'GBDEVU63Y6NTHJQQZIKVTC23NWLQVP3WJ2RI2OTSJTNYOIGICST6DUXR',
+  instructions: 'https://apay.io/',
+});
+directory.addAsset('apay.io', {
+  code: 'ZRX',
+  issuer: 'GBDEVU63Y6NTHJQQZIKVTC23NWLQVP3WJ2RI2OTSJTNYOIGICST6DUXR',
+  instructions: 'https://apay.io/',
+});
+directory.addAsset('apay.io', {
+  code: 'KIN',
+  issuer: 'GBDEVU63Y6NTHJQQZIKVTC23NWLQVP3WJ2RI2OTSJTNYOIGICST6DUXR',
+  instructions: 'https://apay.io/',
+});
+directory.addAsset('apay.io', {
+  code: 'LINK',
+  issuer: 'GBDEVU63Y6NTHJQQZIKVTC23NWLQVP3WJ2RI2OTSJTNYOIGICST6DUXR',
+  instructions: 'https://apay.io/',
+});
+directory.addAsset('apay.io', {
+  code: 'MTL',
+  issuer: 'GBDEVU63Y6NTHJQQZIKVTC23NWLQVP3WJ2RI2OTSJTNYOIGICST6DUXR',
+  instructions: 'https://apay.io/',
+});
+directory.addAsset('apay.io', {
+  code: 'SALT',
+  issuer: 'GBDEVU63Y6NTHJQQZIKVTC23NWLQVP3WJ2RI2OTSJTNYOIGICST6DUXR',
+  instructions: 'https://apay.io/',
+});
+
+directory.addAnchor({
+  domain: 'mobius.network',
+  website: 'https://mobius.network/',
+  logo: 'mobius.network',
+  color: '#6f39fc',
+  displayName: 'Mobius',
+});
+directory.addAsset('mobius.network', {
+  code: 'MOBI',
+  issuer: 'GA6HCMBLTZS5VYYBCATRBRZ3BZJMAFUDKYYF6AH6MVCMGWMRDNSWJPIH',
+  type: 'token',
+});
+
+directory.addAnchor({
+  domain: 'flutterwave.com',
+  website: 'https://flutterwave.com/',
+  logo: 'flutterwave.com',
+  color: '#febe12',
+  displayName: 'Flutterwave',
+});
+directory.addAsset('flutterwave.com', {
+  code: 'NGN',
+  issuer: 'GCC4YLCR7DDWFCIPTROQM7EB2QMFD35XRWEQVIQYJQHVW6VE5MJZXIGW',
+});
+
+directory.addAnchor({
+  domain: 'collective21.org',
+  website: 'https://collective21.org/',
+  logo: 'collective21.org',
+  color: '#3abfce',
+  displayName: 'Collective21',
+});
+directory.addAsset('collective21.org', {
+  code: 'SEED',
+  issuer: 'GDPFSEBZO2W4TLWZO7FIMMG3QONHXYVF6LUULI6HUJS6PJLE4TRZEXLF',
+});
+
+directory.addAnchor({
+  domain: 'liquido.i-server.org',
+  website: 'https://liquido.i-server.org/',
+  logo: 'liquido.i-server.org',
+  displayName: 'Liquido',
+});
+directory.addAsset('liquido.i-server.org', {
+  code: 'XLQ',
+  issuer: 'GD2RRX6BKVTORZ6RIMBLWFVUOAYOLTS2QFJQUQPXLI3PBHR3TMLQNGZX',
+});
+
+directory.addAnchor({
+  domain: 'moni.com',
+  website: 'https://moni.com/',
+  logo: 'moni.com',
+  displayName: 'MONI',
+});
+directory.addAsset('moni.com', {
+  code: 'EUR',
+  issuer: 'GAKBPBDMW6CTRDCXNAPSVJZ6QAN3OBNRG6CWI27FGDQT2ZJJEMDRXPKK',
+});
+
+directory.addAnchor({
+  domain: 'tonaira.com',
+  website: 'https://tonaira.com/',
+  logo: 'tonaira.com',
+  color: '#82be37',
+  displayName: 'Tonaira',
+});
+directory.addAsset('tonaira.com', {
+  code: 'NGN',
+  issuer: 'GCLRUZDCWBHS7VIFCT43BARPP63BHR32HMEVKXYQODA5BU6SIGFK4HL2',
+});
+
+directory.addAnchor({
+  domain: 'equid.co',
+  website: 'https://equid.co/',
+  logo: 'equid.co',
+  displayName: 'eQuid',
+});
+directory.addAsset('equid.co', {
+  code: 'EQD',
+  issuer: 'GCGEQJR3E5BVMQYSNCHPO6NPP3KOT4VVZHIOLSRSNLE2GFY7EWVSLLTN',
+});
+
+directory.addAnchor({
+  domain: 'xirkle.com',
+  website: 'https://xirkle.com/',
+  logo: 'xirkle.com',
+  color: '#ffbb00',
+  displayName: 'Xirkle',
+});
+directory.addAsset('xirkle.com', {
+  code: 'XIR',
+  issuer: 'GAO4DADCRAHA35GD6J3KUNOB5ELZE5D6CGPSJX2WBMEQV7R2M4PGKJL5',
+  type: 'token',
+});
+
+directory.addAnchor({
+  domain: 'golix.io',
+  website: 'https://golix.io/',
+  logo: 'golix.io',
+  displayName: 'Golix',
+});
+directory.addAsset('golix.io', {
+  code: 'USD',
+  issuer: 'GCYK67DDGBOANS6UODJ62QWGLEB2A7JQ3XUV25HCMLT7CI23PMMK3W6R',
+});
+directory.addAsset('golix.io', {
+  code: 'BTC',
+  issuer: 'GCYK67DDGBOANS6UODJ62QWGLEB2A7JQ3XUV25HCMLT7CI23PMMK3W6R',
+});
+
+directory.addAnchor({
+  domain: 'cryptomover.com',
+  website: 'https://cryptomover.com/',
+  logo: 'cryptomover.com',
+  color: '#fbd364',
+  displayName: 'Cryptomover',
+});
+directory.addAsset('cryptomover.com', {
+  code: 'CM3',
+  issuer: 'GDU2FEL6THGGOFDHHP4I5FHNWY4S2SXYUBCEDB5ZREMD6UFRT4SYWSW2',
+});
+directory.addAsset('cryptomover.com', {
+  code: 'CM10',
+  issuer: 'GDBCHKTHJUKDGSIQSTBUXFWVP3QVART5LED6KRZQ5X4Z5WLT4BGYXWCI',
+});
+directory.addAsset('cryptomover.com', {
+  code: 'CMA',
+  issuer: 'GBWZHAVWY23QKKDJW7TXLSIHY5EX4NIB37O4NMRKN2SKNWOSE5TEPCY3',
+});
+directory.addAsset('cryptomover.com', {
+  code: 'CMS',
+  issuer: 'GCVBUIXKKLH2DYHZRSLZUIZSVJUL74RTW6FVCCEYB2OE3RH7RVDBPCFG',
+});
+directory.addAsset('cryptomover.com', {
+  code: 'HKDT',
+  issuer: 'GABSZVZBYEO5F4V5LZKV7GR4SAJ5IKJGGOF43BIN42FNDUG7QPH6IMRQ',
+});
+
+directory.addAnchor({
+  domain: 'thefutbolcoin.io',
+  website: 'https://thefutbolcoin.io/',
+  logo: 'thefutbolcoin.io',
+  color: '#4b914e',
+  displayName: 'TFC',
+});
+directory.addAsset('thefutbolcoin.io', {
+  code: 'TFC',
+  issuer: 'GDS3XDJAA4VY6MJYASIGSIMPHZ7AQNZ54RKLWT7MWCOU5YKYEVCNLVS3',
+});
+
+directory.addAnchor({
+  domain: 'anclax.com',
+  website: 'https://anclax.com',
+  logo: 'anclax.com',
+  color: '#eeeeee',
+  displayName: 'AnclaX',
+});
+
+directory.addAsset('anclax.com', {
+  code: 'COP',
+  issuer: 'GAEDLNMCQZOSLY7Y4NW3DTEWQEVVCXYYMBDNGPPGBMQH4GFYECBI7YVK',
+  instructions: 'https://anclax.com',
+  type: 'iou',
+});
+
+directory.addAnchor({
+  domain: 'astral9.io',
+  website: 'https://astral9.io',
+  logo: 'astral9.io',
+  color: '#000000',
+  displayName: 'XA9',
+});
+
+directory.addAsset('astral9.io', {
+  code: 'XA9',
+  issuer: 'GAUWES2POH347NNJGRI4OBM34LMOCMWSTZ3RAOZMOBH4PFVBWFMHLNTC',
+  type: 'token',
+});
+
+
+directory.addAnchor({
+  domain: 'winsome.gift',
+  website: 'https://winsome.gift',
+  logo: 'winsome.gift',
+  color: '#4671c4',
+  displayName: 'WIN',
+});
+
+directory.addAsset('winsome.gift', {
+  code: 'WIN',
+  issuer: 'GCNHYZLBCSVZHSQJ2DOIBHYBF4J24DJYGS5QKURX4AGSLBK6SDJOYWIN',
+  type: 'token',
+});
+
+ directory.addAnchor({
    domain: 'cowrie.exchange',
    website: 'https://cowrie.exchange',
    logo: 'cowrie.exchange',
    color: '#2a1c4a',
    displayName: 'Cowrie Exchange',
-});
-directory.addAsset('cowrie.exchange', {
+ });
+ directory.addAsset('cowrie.exchange', {
    code: 'NGNT',
    issuer: 'GAWODAROMJ33V5YDFY3NPYTHVYQG7MJXVJ2ND3AOGIHYRWINES6ACCPD',
    instructions: 'https://cowrie.exchange',
    type: 'token',
+ });
+
+directory.addAnchor({
+  domain: 'pr.network',
+  website: 'https://pr.network',
+  logo: 'pr.network',
+  color: '#35c982',
+  displayName: 'XPR',
+  wildcardIssuer: 'GAZPKDTEZ5UM3BF4E7FL7EMXRMLH76F2TNVXRLOF6SCVXOFWSPCEWFI5',
 });
+directory.addAsset('pr.network', {
+  code: 'XPR',
+  issuer: 'GAZPKDTEZ5UM3BF4E7FL7EMXRMLH76F2TNVXRLOF6SCVXOFWSPCEWFI5',
+  type: 'token',
+});
+directory.addWildcard('pr.network', {
+  issuer: 'GAZPKDTEZ5UM3BF4E7FL7EMXRMLH76F2TNVXRLOF6SCVXOFWSPCEWFI5',
+});
+
 // Pairs involving "fiat" assets
 // "Fiat" asset should be counterSelling
 directory.addPair({
